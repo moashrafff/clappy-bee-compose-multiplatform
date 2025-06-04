e8a08f8a7d1232c3d76c571e83f9c0a85a42f816
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-06-04 17:51:18 +0300

    implement audio for Web

--- composeApp/src/wasmJsMain/kotlin/com/moashraf/beegame/domain/AudioPlayer.kt
+++ composeApp/src/wasmJsMain/kotlin/com/moashraf/beegame/domain/AudioPlayer.kt
@@ -1,11 +1,79 @@
 package com.moashraf.beegame.domain
 
-actual class AudioPlayer() {
-    actual fun playGameOverSound(){}
-    actual fun playJumpSound() {}
-    actual fun playFallingSound() {}
-    actual fun stopFallingSound() {}
-    actual fun playGameSoundInLoop() {}
-    actual fun stopGameSound() {}
-    actual fun release() {}
+import org.w3c.dom.Audio
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class AudioPlayer {
+    private val audioElements = mutableMapOf<String, Audio>()
+
+    actual fun playGameOverSound() {
+        stopFallingSound()
+        playSound(fileName = "game_over.wav")
+    }
+
+    actual fun playJumpSound() {
+        stopFallingSound()
+        playSound(fileName = "jump.wav")
+    }
+
+    actual fun playFallingSound() {
+        playSound(fileName = "falling.wav")
+    }
+
+    actual fun stopFallingSound() {
+        stopSound(fileName = "falling.wav")
+    }
+
+    actual fun playGameSoundInLoop() {
+        playSound(fileName = "game_sound.wav", loop = true)
+    }
+
+    actual fun stopGameSound() {
+        playGameOverSound()
+        stopSound(fileName = "game_sound.wav")
+    }
+
+    actual fun release() {
+        stopAllSounds()
+        audioElements.clear()
+    }
+
+    private fun playSound(
+        fileName: String,
+        loop: Boolean = false
+    ) {
+        val audio = audioElements[fileName] ?: createAudioElement(fileName).also {
+            audioElements[fileName] = it
+        }
+        audio.loop = loop
+        audio.play().catch {
+            println("Error playing sound: $fileName")
+            it
+        }
+    }
+
+    private fun stopSound(fileName: String) {
+        audioElements[fileName]?.let { audio ->
+            audio.pause()
+            audio.currentTime = 0.0
+        }
+    }
+
+    private fun stopAllSounds() {
+        audioElements.values.forEach { audio ->
+            audio.pause()
+            audio.currentTime = 0.0
+        }
+    }
+
+    private fun createAudioElement(fileName: String): Audio {
+        val path = "composeResources/beegame.composeapp.generated.resources/files/$fileName"
+//        val path = "src/commonMain/composeResources/files/$fileName"
+        return Audio(path).apply {
+            onerror = { _, _, _, _, _ ->
+                println("Error loading audio file: $path")
+                null
+            }
+        }
+    }
 }
